383d3e0e6becfee2c6f8be3ba771e55f53b6a7ea
jdarmada <jdarmada@gmail.com>
2023-11-01 20:22:12 -0700

    last push

--- pepper-deck/src/components/InterviewItem.js
+++ pepper-deck/src/components/InterviewItem.js
@@ -1,12 +1,13 @@
 
+
 import './InterviewItem.css'
 
-const InterviewItem = () => {
+const InterviewItem = (props) => {
  return(
-    <div className="itemContainer">
-      <div className="companyNameTitle">Facebook</div>
-      <div className="interviewDate">10/31</div>
-      <div className="positionTitle">Front-end Engineer</div>
+    <div className="itemContainer relative flex w-96 rounded-md bg-white bg-clip-border text-gray-700 shadow-md">
+      <div className="interviewDate">{props.date}</div>
+      <div className="companyNameTitle">{props.company}</div>
+      <div className="positionTitle">{props.position}</div>
     </div>
  )
 }
--- pepper-deck/src/pages/Home.js
+++ pepper-deck/src/pages/Home.js
@@ -1,16 +1,42 @@
 import InterviewItem from "../components/InterviewItem";
 import './Home.css'
+import Icon from '../assets/icons8-add-new-50.png'
 
 function Home() {
+  const example = [
+    {date: '10/31',
+     company: 'Facebook',
+     position: 'front-end Engineer'},
+     {date: '12/10',
+     company: 'Oracle',
+     position: 'back-end Engineer'}, 
+     {date: '12/25',
+     company: 'IBM',
+     position: 'Front-end Engineer'},
+
+  ]
+  for(let i = 0; i<example.length; i++){
+     <InterviewItem props = {example[i]}></InterviewItem>
+  }
+  
   return (
     <div className="bodyContainer">
 
       <div class = 'titleContainer'>
         <h2 class = 'homeTitle' > Upcoming Interviews</h2>
       </div>
+      <div className="categories">
+        <h2>Interview Date</h2>
+        <h2>Company</h2>
+        <h2>Position</h2>
+      </div>
       <div className="interviewsContainer">
-        <InterviewItem/>
-
+        {<InterviewItem/>}
+      </div>
+      <div className = "addIntBtnContainer">
+        <button className = "addIntBtn  ">
+          <img className = 'btnImage' src = {Icon} alt = "Add Interview" ></img>
+        </button>
       </div>
     </div>
   );
